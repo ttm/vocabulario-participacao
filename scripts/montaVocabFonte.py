3dc52175b2c9877005864be15dee4933a64af0c3
Renato Fabbri <renato.fabbri@gmail.com>
2014-09-15 02:20:34 -0300

    feitas versoes iniciais dos vocabularios

--- scripts/montaVocabFonte.py
+++ scripts/montaVocabFonte.py
@@ -13,7 +13,7 @@ foo= ET.SubElement(root, "isComposedBy")
 conta_id=1
 labels={'vocabIPEA.txt':u'Vocabulário repassado pelo IPEA',
  'vocabOPS.txt':u'Vocabulário observado na OPS',
- 'vocabFreqParticipa.txt':u'Vocabulário com anta incidência no Participa.br',
+ 'vocabFreqParticipa.txt':u'Vocabulário com bastante incidência no Participa.br',
  'vocabOPA.txt':u'Vocabulário observado na OPA',
  'vocabPNPS.txt':u'Vocabulário observado no Decreto nº 8243/14 (PNPS)'}
 for arquivo in arquivos:
@@ -22,12 +22,16 @@ for arquivo in arquivos:
     termos_=tfile.readlines()
     fonte= ET.SubElement(foo, "node")
     fonte.set("label",labels[arquivo])
+    fonte.set("id",arquivo.split(".txt")[0])
     foo2= ET.SubElement(fonte, "isComposedBy")
     for palavra in termos_:
         termo = ET.SubElement(foo2, "node")
         termo.set("id",str(conta_id))
         conta_id+=1
-        termo.set("label",palavra[:-1].decode("utf-8"))
+        # Para deixar em caixa alta somente as palavras mais
+        # significativas, conforme modelo recebido
+        palavra_=" ".join(w.capitalize() if w not in ["de","e","da","do","na","no"] else w for w in palavra[:-1].split())
+        termo.set("label",palavra_.decode("utf-8"))
 tree = ET.ElementTree(root)
 tree.write('vp-fonte.xml', xml_declaration=True, encoding='utf-8')
 # para deixar bem formatado:
--- scripts/montaVocabLista.py
+++ scripts/montaVocabLista.py
@@ -20,7 +20,10 @@ for palavra in termos:
     termo = ET.SubElement(foo, "node")
     termo.set("id",str(conta_id))
     conta_id+=1
-    termo.set("label",palavra[:-1].decode("utf-8"))
+    # Para deixar em caixa alta somente as palavras mais
+    # significativas, conforme modelo recebido
+    palavra_=" ".join(w.capitalize() if w not in ["de","e","da","do","na","no"] else w for w in palavra[:-1].split())
+    termo.set("label",palavra_.decode("utf-8"))
 tree = ET.ElementTree(root)
 tree.write('vp-lista.xml', xml_declaration=True, encoding='utf-8')
 # para deixar bem formatado:
