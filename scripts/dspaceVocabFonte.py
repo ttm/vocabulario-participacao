a1f3bd89d9df7ccd2340f734d0c77412f8eb4428
Renato Fabbri <renato.fabbri@gmail.com>
2014-10-28 04:22:20 -0200

    comecado hack para fazer vocabulario com as fontes

---
+++ scripts/dspaceVocabFonte.py
@@ -0,0 +1,71 @@
+#!python3
+#-*- coding: utf-8 -*-
+import xml.etree.ElementTree as ET, os
+import rdflib as r
+def G(S,P,O):
+    g.add((S,P,O))
+L=r.Literal
+
+vbs = r.Namespace("http://purl.org/socialparticipation/vbs/")
+rdf = r.namespace.RDF
+rdfs = r.namespace.RDFS
+skos = r.namespace.SKOS
+xsd = r.namespace.XSD
+
+store="../rdf/vbsConferencia.rdf"
+store2="../rdf/vbsConselho.rdf"
+store3="../rdf/vbsOuvidoria.rdf"
+g = r.Graph()
+g.load(store)
+g.load(store2)
+g.load(store3)
+
+PREFIX="""PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"""
+
+q="SELECT ?l WHERE {?s a skos:Concept. ?s skos:prefLabel ?l . }"
+res=g.query(PREFIX+q)
+conceitos=[rr[0] for rr in res]
+conceitos=sorted(conceitos,key=lambda s: s.replace(u"á",u"a").replace(u"Á",u"a").replace(u"Ó",u"o").replace(u"ó",u"o").encode("utf8").lower())
+
+#f=open("../rdf/vbsConferenciaPalavras.txt", "wb")
+#sys.stdout = f
+for cc in conceitos:
+    print (u"%s"%(cc,)).encode("utf8")
+
+
+
+
+# leitura dos termos dos arquivos na pasta ../fontes/
+#arquivos=os.listdir("../fontes/")
+#arquivos=[ar for ar in arquivos if ar.endswith("txt")]
+
+root = ET.Element('node')
+root.set("id","vocab_part")
+root.set("label",u"Vocabulário Controlado para Biblioteca de Participação Social")
+foo= ET.SubElement(root, "isComposedBy")
+conta_id=1
+#labels={'vocabIPEA.txt':u'Vocabulário repassado pelo IPEA',
+# 'vocabOPS.txt':u'Vocabulário observado na OPS',
+# 'vocabFreqParticipa.txt':u'Vocabulário com bastante incidência no Participa.br',
+# 'vocabOPA.txt':u'Vocabulário observado na OPA',
+# 'vocabPNPS.txt':u'Vocabulário observado no Decreto nº 8243/14 (PNPS)'}
+for arquivo in arquivos:
+    path="../fontes/"+arquivo
+    tfile=open(path,"rb")
+    termos_=tfile.readlines()
+    fonte= ET.SubElement(foo, "node")
+    fonte.set("label",labels[arquivo])
+    fonte.set("id",arquivo.split(".txt")[0])
+    foo2= ET.SubElement(fonte, "isComposedBy")
+    for palavra in termos_:
+        termo = ET.SubElement(foo2, "node")
+        termo.set("id",str(conta_id))
+        conta_id+=1
+        # Para deixar em caixa alta somente as palavras mais
+        # significativas, conforme modelo recebido
+        palavra_=" ".join(w.capitalize() if w not in ["de","e","da","do","na","no",u"à"] else w for w in palavra[:-1].split())
+        termo.set("label",palavra_.decode("utf-8"))
+tree = ET.ElementTree(root)
+tree.write('../vocabulario/dspaceMec.xml', xml_declaration=True, encoding='utf-8')
+# para deixar bem formatado:
+# ! xmllint --format - 
